7c82246121382b1950485e1fe847877ba943ee10
Frank Liu <fliu@proserveit.com>
2023-07-31 21:47:46 -0400

    18. Generate JWT Token with JWT Token Handler

---
+++ Web API/Controllers/AuthController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Web_API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpPost]
+        public IActionResult Authenticate([FromBody]Credential credential)
+        {
+            // Verify the credential
+            if (credential.UserName == "admin" && credential.Password == "password")
+            {
+                // Creating the security context
+                var claims = new List<Claim> {
+                    new Claim(ClaimTypes.Name, "admin"),
+                    new Claim(ClaimTypes.Email, "admin@mywebsite.com"),
+                    new Claim("Department", "HR"),
+                    new Claim("Admin", "true"),
+                    new Claim("Manager", "true"),
+                    new Claim("EmploymentDate", "2023-01-01")
+                };
+
+                var expiresAt = DateTime.UtcNow.AddMinutes(10);
+
+                return Ok(new
+                {
+                    access_token = CreateToken(claims, expiresAt),
+                    expires_at = expiresAt
+                });
+                
+            }
+
+            ModelState.AddModelError("Unauthorized", "You are not authorized to access the endpoint.");
+            return Unauthorized(ModelState);
+        }
+
+        private string CreateToken(IEnumerable<Claim> claims, DateTime expireAt)
+        {
+            var secretKey = Encoding.ASCII.GetBytes(configuration.GetValue<string>("SecretKey")??"");
+
+            // generate the JWT
+            var jwt = new JwtSecurityToken(
+                    claims: claims,
+                    notBefore: DateTime.UtcNow,
+                    expires: expireAt,
+                    signingCredentials: new SigningCredentials(
+                        new SymmetricSecurityKey(secretKey),
+                        SecurityAlgorithms.HmacSha256Signature)
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(jwt);
+        }
+
+    }
+    
+    public class Credential
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
