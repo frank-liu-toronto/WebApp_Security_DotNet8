33b6f4417990e4a5bc13f3f2becf1e4d2359ea8c
Frank Liu <fliu@proserveit.com>
2023-08-18 11:03:52 -0400

    25. Create the Database for Identity

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using WebApp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
 
 var app = builder.Build();
 
