9f0ee0a1d053987148ba0f3460bd49ec44df453c
Frank Liu <fliu@proserveit.com>
2023-08-20 15:28:04 -0400

    36. Sign Out

---
+++ WebApp/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages.Account
+{
+    public class LogoutModel : PageModel
+    {
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public LogoutModel(SignInManager<IdentityUser> signInManager)
+        {
+            this.signInManager = signInManager;
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToPage("/Account/Login");
+        }
+    }
+}
